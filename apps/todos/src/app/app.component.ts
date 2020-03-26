8b7f8ca6f86610efc32eb74f01d712d62a9ea2ce
Johannes Schobel <johannes.schobel@googlemail.com>
2020-03-26 17:08:13 +0100

    feat: add angular app

---
+++ apps/todos/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nrwl-xplat-ionic5-example-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'todos';
+}
